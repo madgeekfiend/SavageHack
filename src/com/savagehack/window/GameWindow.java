39f69e077cd2c351ea2e84cc0e0b350a20774a4f
Sam <sam.contapay@gmail.com>
2011-07-06 08:16:08 -0700

    Redid the layout with JScrollPanes for both the player info and the ASCII map. Representing map with ASCII text for now.

--- src/com/savagehack/window/GameWindow.java
+++ src/com/savagehack/window/GameWindow.java
@@ -2,18 +2,18 @@ package com.savagehack.window;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextPane;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.swing.JMenu;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextPane;
 
 /**
  * This is the main game window for SavageHack. A roguelike game that uses the
@@ -46,41 +46,37 @@ public class GameWindow extends JFrame {
 	private final JMenuBar menuBar = new JMenuBar();
 	private final JMenu mnFile = new JMenu("File");
 	private final JMenuItem mntmExit = new JMenuItem("Exit");
+	private final JSplitPane splitPane = new JSplitPane();
+	private final JSplitPane splitPanePlayer = new JSplitPane();
+	private final JTextPane ASCIIMap = new JTextPane();
 	
 	public GameWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("SavageHack");
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{244, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{295, 100};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{1.0, 0.0};
+		gridBagLayout.columnWidths = new int[]{244, 0};
+		gridBagLayout.rowHeights = new int[]{0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0};
 		getContentPane().setLayout(gridBagLayout);		
 		
 		characterControlPanel.setBorder( BorderFactory.createTitledBorder("Player") );
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 5, 5);
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		getContentPane().add(characterControlPanel, gbc_panel);
-				
-		GridBagConstraints gbc_textPane = new GridBagConstraints();
-		gbc_textPane.insets = new Insets(0, 0, 0, 5);
-		gbc_textPane.fill = GridBagConstraints.BOTH;
-		gbc_textPane.gridx = 0;
-		gbc_textPane.gridy = 1;
 		logPane.setEditable(false);
-		getContentPane().add(logPane, gbc_textPane);
-		
-		ScrollPane scrollPane = new ScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.gridwidth = 3;
-		gbc_scrollPane.gridheight = 2;
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 1;
-		gbc_scrollPane.gridy = 0;		
-		getContentPane().add(scrollPane, gbc_scrollPane);
+		splitPanePlayer.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		splitPanePlayer.setTopComponent( characterControlPanel );
+		splitPanePlayer.setBottomComponent( logPane );
+		splitPanePlayer.setDividerLocation( 350 );
+			
+		JScrollPane scrollPane = new JScrollPane( ASCIIMap );	
+		// Now let's add some stuff
+		GridBagConstraints gbc_splitPane = new GridBagConstraints();
+		gbc_splitPane.fill = GridBagConstraints.BOTH;
+		gbc_splitPane.gridx = 0;
+		gbc_splitPane.gridy = 0;
+		splitPane.setRightComponent( scrollPane );
+		splitPane.setLeftComponent( splitPanePlayer );
+		//splitPane.setDividerLocation(200);
+		getContentPane().add(splitPane, gbc_splitPane);
 		
 		setJMenuBar(menuBar);
 		
